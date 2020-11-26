43c1a79e4cd6716a1a4968bffb78913c76896045
Emmanuel De Leon <deleon.lerma.17163@itsmante.edu.mx>
2020-11-26 00:14:45 -0600

    pantalla mostrar respuestas al administrador

--- src/components/admin/questionsAdmin/AnswerQuestionAdminScreen.js
+++ src/components/admin/questionsAdmin/AnswerQuestionAdminScreen.js
@@ -1,9 +1,56 @@
 import React from 'react'
+import './answer-question-admin.scss'
+import imagen from './../../../assets/images/mexico-imagen.jpg'
+import { Link } from 'react-router-dom';
+
 
 export const AnswerQuestionAdminScreen = () => {
     return (
         <div>
-            
+            <div className=" titulo-pregunta container-fluid mt-2">
+                <div className="row  justify-content-center">
+                    <h1>¿Cuandos estados tiene México?</h1>
+                </div>
+            </div>
+            <h3>1 de 5</h3>
+
+            <div className="tiempo">
+                <h1>15</h1>
+            </div>
+
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="mt-2">
+                        <img className="container-imagen" src={imagen}></img>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container-fluid mt-4">
+                <div className="row">
+                    <div className="col-6">
+                        <button className="respuesta1 mt-2"> <i class="fas fa-caret-up"></i>  45 </button>
+                        <button className="respuesta2 mt-2"> <i class="fas fa-circle"></i>  32 </button>
+                    </div>
+
+                    <div className="col-6">
+                        <button className="respuesta3 mt-2"><i class="fas fa-square"></i> 29 </button>
+                        <button className="respuesta4 mt-2"><i class="fas fa-adjust"></i> 34 </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="numero-respuestas">
+                <h2>1</h2>
+                <p>Respuesta</p>
+            </div>
+
+            <Link to="/question" className="siguiente-pregunta">Saltar </Link>
+
+
         </div>
+
+
+        
     )
 }
